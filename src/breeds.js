68ce98024763e6526251f2a705c52761b54054a7
Ben Schinn <ben.schinn@mx.com>
2020-12-09 18:38:21 -0500

    fix typo and replace cocker as it is getting 404 from dog api

--- src/breeds.js
+++ src/breeds.js
@@ -3,5 +3,6 @@ export const breeds = [
   'affenpinscher',
   'australian',
   'mexicanhairless',
-  'cocker',
+  'doberman',
+  'chihuahua'
 ]
--- src/index.js
+++ src/index.js
@@ -48,7 +48,7 @@ function dogCardMaker({ imageURL, breed }) {
 
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
-//    * Proyects with npm: install it with npm and import it into this file
+//    * Projects with npm: install it with npm and import it into this file
 
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
