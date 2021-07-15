709edfaa1123de6a887169b6c120a2d83cf375ac
Timothy Roy <timothy.roy@protonmail.ch>
2021-07-14 22:28:40 -0500

    Added axios and used it to fetch data, then built components with that data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Imports at the top of the file!
 // We never nest imports inside blocks of code!
-
+import axios from 'axios'
 
 // 👉 TASK 1- Test out the following endpoints:
 
@@ -16,10 +16,42 @@
 //  * With Chrome and the Network Tab
 //  * With JS using the native fetch [STRETCH]
 
+// how to explore an API
+// "hit the endpoint" by putting the URL into Chrome, or by using fetch, or by using Postman
+// look at the data you get back
+// that way you know the shape of the object, or array, etc
+
+// fetch this url:
+//  https://lambda-times-api.herokuapp.com/quotes
+// get out the data
+// console log the data
+const quotesPromise = fetch('https://lambda-times-api.herokuapp.com/quotes')
+
+quotesPromise
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+const dogPromise = fetch('https://dog.ceo/api/breed/retriever/images/random/5')
+dogPromise
+  .then(response => response.json())
+  .then(dogData => console.log(dogData)) // make components inside the .then
+
+// can I use the dogData outside of the .then??
+// console.log(dogData)
+
+const dogMockData =[
+  "https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3315.jpg",
+  "https://images.dog.ceo/breeds/retriever-chesapeake/n02099849_3753.jpg",
+  "https://images.dog.ceo/breeds/retriever-curly/n02099429_282.jpg",
+  "https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_5306.jpg",
+  "https://images.dog.ceo/breeds/retriever-golden/n02099601_3414.jpg"
+] 
+
 
 // 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
-const entryPoint = null
+// Where to append our components, after we have made them?
+const entryPoint = document.querySelector(".entry");
 
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
@@ -45,20 +77,49 @@ function dogCardMaker({ imageURL, breed }) {
   return dogCard
 }
 
+// how do we use our data to make components and attach the components to the DOM?
+
+// loop over our data
+// dogMockData.forEach(mockUrl => {
+// // use each member of our data to create a component
+//   const dogCard = dogCardMaker({ breed: 'retriever', imageURL: mockUrl })
+// // attach the component to the DOM
+//   entryPoint.appendChild(dogCard)
+// })
+
 
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
-//    * Proyects with npm: install it with npm and import it into this file
+//    * Projects with npm: install it with npm and import it into this file
+      // where to look to know if we need to install axios?
+
+// const quotesPromise = fetch('https://lambda-times-api.herokuapp.com/quotes')
+axios
+  .get('https://lambda-times-api.herokuapp.com/quotes')
+  .then(response => console.log(response.data)) // look ma, no response.json()!
+  .catch(drama => console.log('handle the drama'))
 
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
-
-
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
+function getDogs(breed, count) {
+    axios
+      .get(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
+      .then(response => {
+         const actualDogUrls = response.data.message
+         actualDogUrls.forEach(dogUrl => {
+         const dogCard = dogCardMaker({ breed, imageURL: dogUrl })
+        entryPoint.append(dogCard)
+         });
+        })
+      .catch(error => console.error(error))
+      .finally(() => console.log('done'))
+}
+getDogs('lhasa', 6)
 
 
 // 👉 (OPTIONAL) TASK 7- Put a button in index.html to 'get dogs' and add a click
